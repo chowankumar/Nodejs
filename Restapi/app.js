57bad1c4f362d508d19a95c54049f8fd2ebdee54
chowankumar <chowanmalhani5@gmail.com>
2024-07-26 18:57:17 +0500

    connect our frontend with backend

---
+++ Restapi/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.send("hi i am live")
+})
+
+
+
+const start = async()=>{
+       try {
+           app.listen(PORT,()=>{
+           console.log( `${PORT} YES I AM CONNECTED`   )        })
+       } catch (error) {
+        console.log(error)
+        
+       }
+}
+
+start()
